13ede77e272a25a9d91b75559d54b029193c0345
Zeyad Sleem <zeyadsleem1@gmail.com>
2025-07-12 12:35:51 +0300

    Add part1 courseinfo

---
+++ part1/courseinfo/src/components/Content.jsx
@@ -0,0 +1,13 @@
+import Part from "./Part"
+
+const Content = ({ part1, part2, part3, exercises1, exercises2, exercises3 }) => {
+    return (
+        <>
+            <Part part={part1} exercises={exercises1} />
+            <Part part={part2} exercises={exercises2} />
+            <Part part={part3} exercises={exercises3} />
+        </ >
+    )
+}
+
+export default Content
